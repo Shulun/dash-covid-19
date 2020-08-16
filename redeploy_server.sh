5eb335e8c8abb37ebbd46aec70d2243cdf1ead87
shulun <chenshulun@gmail.com>
2020-08-16 01:41:53 -0700

    Added backspace to execute nohup command in redeploy script

--- redeploy_server.sh
+++ redeploy_server.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+str=$"\n"
 pid=$(ps -ef | grep "python /root/dash-covid-19/app.py" | grep -v grep | awk '{print $2}' )
 kill 9 $pid
 nohup python /root/dash-covid-19/app.py &
+sstr=$(echo -e $str)
+echo $sstr
